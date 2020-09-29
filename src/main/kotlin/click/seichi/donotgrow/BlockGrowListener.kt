0007572e381aa1b5a3ea96456be8f885c145f625
Lucky3028 <lucky3028.0@gmail.com>
2020-09-29 22:57:23 +0900

    [Remove] 不要な変数

--- src/main/kotlin/click/seichi/donotgrow/BlockGrowListener.kt
+++ src/main/kotlin/click/seichi/donotgrow/BlockGrowListener.kt
@@ -11,8 +11,7 @@ object BlockGrowListener : Listener {
     fun onCactusGrow(event: BlockGrowEvent) {
         // event.getBlockで得られるのはMaterial.AIR（=成長するサボテンが出現する座標のブロック）なので、Y座標を-1してMaterial.CACTUSかどうかを判定
         val loc = event.block.location.apply { this.y -= 1 }
-        val block = loc.block
 
-        if (block.type == Material.CACTUS) event.isCancelled = true
+        if (loc.block.type == Material.CACTUS) event.isCancelled = true
     }
 }
